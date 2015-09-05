0f9d090f340e008e666e525ad526d2266b9798b5
Simon <simon@slicksim.co.uk>
2015-09-05 08:33:39 +0100

    Updated remotemodel to allow pasing of options and parameters for the request object

--- slick.remotemodel.js
+++ slick.remotemodel.js
@@ -1,173 +1,208 @@
 (function ($) {
-  /***
-   * A sample AJAX data store implementation.
-   * Right now, it's hooked up to load Hackernews stories, but can
-   * easily be extended to support any JSONP-compatible backend that accepts paging parameters.
-   */
-  function RemoteModel() {
-    // private
-    var PAGESIZE = 50;
-    var data = {length: 0};
-    var searchstr = "";
-    var sortcol = null;
-    var sortdir = 1;
-    var h_request = null;
-    var req = null; // ajax request
+    /***
+	 * A simple, sample AJAX data store implementation.
+	 *
+	 * This can be used as is for a basic JSONP-compatible backend that accepts paging parameters.
+	 */
+    function RemoteModel(options, requestParams) {
+        // private
+        var data = { length: 0 };
+        var req = null;
+        var h_req = null;
 
-    // events
-    var onDataLoading = new Slick.Event();
-    var onDataLoaded = new Slick.Event();
+        var default_options = {
+            pagesize: 50,
+            url: '',
+            method: 'jsonp',
+            response_item: ''
+        };
+        var options = $.extend(default_options, options);
 
+        // Events
+        var onDataLoading = new Slick.Event();
+        var onDataLoaded = new Slick.Event();
 
-    function init() {
-    }
-
-
-    function isDataLoaded(from, to) {
-      for (var i = from; i <= to; i++) {
-        if (data[i] == undefined || data[i] == null) {
-          return false;
+        var default_request_params = {
+            jsonp: {
+                callbackParameter: "callback",
+                cache: true,
+            },
+            ajax: {
+                dataType: 'json',
+            },
+            '*': {
+                success: onSuccess,
+                error: function () {
+                    onError(fromPage, toPage)
+                }
+            }
         }
-      }
 
-      return true;
+        var request_params = $.extend(default_request_params, requestParams);
+
+        function init() { }
+
+        function isDataLoaded(from, to) {
+            for (var i = from; i <= to; i++) {
+                if (data[i] == undefined || data[i] == null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        function clear() {
+            for (var key in data) {
+                delete data[key];
+            }
+            data.length = 0;
+            return this;
+        }
+
+        function ensureData(from, to) {
+            if (req) {
+                req.abort();
+                for (var i = req.fromPage; i <= req.toPage; i++) {
+                    data[i * options.pagesize] = undefined;
+                }
+            }
+
+            if (from < 0) {
+                from = 0;
+            }
+
+            var fromPage = Math.floor(from / options.pagesize);
+            var toPage = Math.floor(to / options.pagesize);
+
+            while (data[fromPage * options.pagesize] !== undefined && fromPage < toPage) {
+                fromPage++;
+            }
+
+            while (data[toPage * options.pagesize] !== undefined && fromPage < toPage) {
+                toPage--;
+            }
+
+            if (fromPage > toPage || ((fromPage == toPage) && data[fromPage * options.pagesize] !== undefined)) {
+                // TODO:  look-ahead
+                return;
+            }
+
+            if (options.url == undefined || options.url == null) {
+                return;
+            }
+
+            var url = options.url(fromPage, toPage, options.pagesize);
+
+            if (h_req != null) {
+                clearTimeout(h_req);
+            }
+
+            h_req = setTimeout(function () {
+                for (var i = fromPage; i <= toPage; i++) {
+                    data[i * options.pagesize] = null; // null indicates a 'requested but not available yet'
+                }
+
+                onDataLoading.notify({ from: from, to: to });
+
+                // Make the AJAX/JSONP call with both the general and specific parameters we need
+                $[options.method]($.extend(request_params['*'], request_params[options.method], {
+                    url: url,
+                    context: {
+                        fromPage: fromPage,
+                        toPage: toPage
+                    }
+                }));
+            }, 50);
+        }
+
+
+        function onError(fromPage, toPage) {
+            throw "Error loading pages " + fromPage + " to " + toPage;
+        }
+
+        function onSuccess(resp) {
+            if (options.response_item == null || options.response_item == '') {
+                options.response_item = 'items';
+                if (typeof resp.items == "undefined" && typeof resp == "object") {
+                    resp.items = resp;
+                }
+            }
+            if (typeof resp.offset == "undefined" || resp.offset == null) {
+                resp.offset = this.fromPage * options.pagesize;
+            }
+
+            if ($.isArray(resp) && typeof resp[0][options.response_item] == "object") {
+                var strategy = objInArray;
+            } else if ($.isArray(resp[options.response_item])) {
+                var strategy = arrayInObj;
+            } else {
+                throw "Could not find '" + options.response_item + "' in JSONP response!"
+            }
+
+            if (typeof resp.count == "undefined" || resp.count == null) {
+                resp.count = strategy.count(resp);
+            }
+
+            for (var i = 0; i < resp.count; i++) {
+                data[resp.offset + i] = strategy.find(resp, i);
+                data[resp.offset + i].index = resp.offset + i;
+            }
+
+            if (typeof resp.total !== "undefined" && !isNaN(resp.total)) {
+                data.length = resp.total;
+            } else {
+                data.length = Math.max(data.length, resp.offset + resp.count);
+            }
+
+            req = null;
+            onDataLoaded.notify({ from: resp.offset, to: resp.offset + resp.count });
+        }
+
+        function reloadData(from, to) {
+            for (var i = from; i <= to; i++) {
+                delete data[i];
+            }
+        }
+
+        function getOptions() {
+            return options;
+        }
+
+        function setOptions(options) {
+            options = $.extend(defaults, options);
+            return this;
+        }
+
+        var objInArray = {
+            find: function (resp, i) { return resp[i][options.response_item]; },
+            count: function (resp) { return resp.length; }
+        };
+
+        var arrayInObj = {
+            find: function (resp, i) { return resp[options.response_item][i]; },
+            count: function (resp) { return resp[options.response_item].length; }
+        };
+
+        init();
+
+        return {
+            // Properties
+            "data": data,
+
+            // Methods
+            "clear": clear,
+            "isDataLoaded": isDataLoaded,
+            "ensureData": ensureData,
+            "reloadData": reloadData,
+            "getOptions": getOptions,
+            "setOptions": setOptions,
+
+            // Events
+            "onDataLoading": onDataLoading,
+            "onDataLoaded": onDataLoaded
+        };
     }
 
-
-    function clear() {
-      for (var key in data) {
-        delete data[key];
-      }
-      data.length = 0;
-    }
-
-
-    function ensureData(from, to) {
-      if (req) {
-        req.abort();
-        for (var i = req.fromPage; i <= req.toPage; i++)
-          data[i * PAGESIZE] = undefined;
-      }
-
-      if (from < 0) {
-        from = 0;
-      }
-
-      if (data.length > 0) {
-        to = Math.min(to, data.length - 1);
-      }
-
-      var fromPage = Math.floor(from / PAGESIZE);
-      var toPage = Math.floor(to / PAGESIZE);
-
-      while (data[fromPage * PAGESIZE] !== undefined && fromPage < toPage)
-        fromPage++;
-
-      while (data[toPage * PAGESIZE] !== undefined && fromPage < toPage)
-        toPage--;
-
-      if (fromPage > toPage || ((fromPage == toPage) && data[fromPage * PAGESIZE] !== undefined)) {
-        // TODO:  look-ahead
-        onDataLoaded.notify({from: from, to: to});
-        return;
-      }
-
-      var url = "http://api.thriftdb.com/api.hnsearch.com/items/_search?filter[fields][type][]=submission&q=" + searchstr + "&start=" + (fromPage * PAGESIZE) + "&limit=" + (((toPage - fromPage) * PAGESIZE) + PAGESIZE);
-
-      if (sortcol != null) {
-          url += ("&sortby=" + sortcol + ((sortdir > 0) ? "+asc" : "+desc"));
-      }
-
-      if (h_request != null) {
-        clearTimeout(h_request);
-      }
-
-      h_request = setTimeout(function () {
-        for (var i = fromPage; i <= toPage; i++)
-          data[i * PAGESIZE] = null; // null indicates a 'requested but not available yet'
-
-        onDataLoading.notify({from: from, to: to});
-
-        req = $.jsonp({
-          url: url,
-          callbackParameter: "callback",
-          cache: true,
-          success: onSuccess,
-          error: function () {
-            onError(fromPage, toPage)
-          }
-        });
-        req.fromPage = fromPage;
-        req.toPage = toPage;
-      }, 50);
-    }
-
-
-    function onError(fromPage, toPage) {
-      alert("error loading pages " + fromPage + " to " + toPage);
-    }
-
-    function onSuccess(resp) {
-      var from = resp.request.start, to = from + resp.results.length;
-      data.length = Math.min(parseInt(resp.hits),1000); // limitation of the API
-
-      for (var i = 0; i < resp.results.length; i++) {
-        var item = resp.results[i].item;
-
-        // Old IE versions can't parse ISO dates, so change to universally-supported format.
-        item.create_ts = item.create_ts.replace(/^(\d+)-(\d+)-(\d+)T(\d+:\d+:\d+)Z$/, "$2/$3/$1 $4 UTC"); 
-        item.create_ts = new Date(item.create_ts);
-
-        data[from + i] = item;
-        data[from + i].index = from + i;
-      }
-
-      req = null;
-
-      onDataLoaded.notify({from: from, to: to});
-    }
-
-
-    function reloadData(from, to) {
-      for (var i = from; i <= to; i++)
-        delete data[i];
-
-      ensureData(from, to);
-    }
-
-
-    function setSort(column, dir) {
-      sortcol = column;
-      sortdir = dir;
-      clear();
-    }
-
-    function setSearch(str) {
-      searchstr = str;
-      clear();
-    }
-
-
-    init();
-
-    return {
-      // properties
-      "data": data,
-
-      // methods
-      "clear": clear,
-      "isDataLoaded": isDataLoaded,
-      "ensureData": ensureData,
-      "reloadData": reloadData,
-      "setSort": setSort,
-      "setSearch": setSearch,
-
-      // events
-      "onDataLoading": onDataLoading,
-      "onDataLoaded": onDataLoaded
-    };
-  }
-
-  // Slick.Data.RemoteModel
-  $.extend(true, window, { Slick: { Data: { RemoteModel: RemoteModel }}});
+    // Slick.Data.RemoteModel
+    $.extend(true, window, { Slick: { Data: { RemoteModel: RemoteModel } } });
 })(jQuery);
